0fb560bc32cb9294634f2c8ce18b222fd934cd35
KaLaMangVT <kalamangvt@gmail.com>
2024-03-06 12:14:42 +0700

    Start my custom NFSW Ranker Launcher

---
+++ GameLauncher/App/Classes/LauncherCore/Global/URLs.cs
@@ -0,0 +1,34 @@
+﻿namespace GameLauncher.App.Classes.LauncherCore.Global
+{
+    class URLs
+    {
+        public static string Main = "http://kalamangworld.servegame.com";
+
+        public static string File = "http://kalamangworld.servegame.com";
+
+        public static string Static = "http://kalamangworld.servegame.com";
+
+        public static string Static_Alt = "http://kalamangworld.servegame.com";
+
+        public static string ModNet = "http://kalamangworld.servegame.com";
+
+        public static string GitHub_Launcher_Stable = "http://kalamangworld.servegame.com/LauncherVersion/GitHub_Launcher_Stable.json";
+
+        public static string GitHub_Launcher_Beta = "http://kalamangworld.servegame.com/LauncherVersion/GitHub_Launcher_Beta.json";
+
+        public static string GitHub_Updater = "http://kalamangworld.servegame.com/LauncherVersion/GitHub_Updater.json";
+
+        public static string[] AntiCheatFD = new string[]
+        {
+            Main + "/report",
+            "https://google.com",
+            "https://google.com"
+        };
+
+        public static string[] AntiCheatSD = new string[]
+        {
+            Main + "/report-manual?",
+            "https://google.com"
+        };
+    }
+}
---
+++ README.md
@@ -0,0 +1,11 @@
+# GameLauncher_NFSW ![Build Status](https://github.com/SoapboxRaceWorld/GameLauncher_NFSW/workflows/GameLauncher%20Reborn%20Build/badge.svg)
+A Rewrite of GameLauncher taken from Need For Speed: World
+
+## Features
+- [X] Game Client Downloader
+- [X] UserSettings.xml Editor
+- [X] Game Files Validator
+- [X] Firewall Checker
+- [X] Windows Defender (Windows 10 Only)
+- [X] Discord Rich Presense / RemoteRPC
+- [X] Self-Signed Code Sign Certificate
